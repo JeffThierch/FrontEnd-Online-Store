1a965963351ce386d4403e172196c24249403dcc
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-23 14:28:11 -0300

    :lipstick: Update: Add new style
    Adicona novos arquivos CSS para melhor experiencia do usuario

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -24,6 +24,7 @@ export default class Categories extends Component {
                 name="userCategory"
                 value={ id }
                 onClick={ handleClick }
+                className="category-radio"
               />
             </label>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import CartIcon from '../assets/CartIcon';
 import '../styles/Header.css';
-import Logo from '../images/store.png';
 
 export default class Header extends Component {
   render() {
     return (
       <header className="header-container">
-        <div>
+        <div className="header-texts">
           <Link to="/" className="header-title">
             <h1>Mercadinho 08</h1>
           </Link>
+          <h2 className="header-bar">|</h2>
+          <h2 className="header-slogan">Aqui você encontra tudo!</h2>
         </div>
         <div>
           <Link to="/cart" data-testid="shopping-cart-button">
