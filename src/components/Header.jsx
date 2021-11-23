fb9db3a407a374bf462b786d7617e3bd29fa1741
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-11-22 22:32:49 -0300

    :lipstick: Feat: Add first styles
    Adiciona os primeiros arquivos css para estilizaçao do site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import CartIcon from '../assets/CartIcon';
+import '../styles/Header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="header-container">
         <div>
-          <Link to="/">
+          <Link to="/" className="header-title">
             <h1>Mercadinho 08</h1>
           </Link>
         </div>
         <div>
           <Link to="/cart" data-testid="shopping-cart-button">
-            <button type="button">
+            <button className="cart-btn" type="button">
               <CartIcon />
             </button>
           </Link>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import '../styles/ProductCard.css';
 
 export default class ProductCard extends Component {
   render() {
     const { searchData, addToCart } = this.props;
     const { title, price, thumbnail, id } = searchData;
     return (
-      <section data-testid="product">
+      <section
+        data-testid="product"
+        className="product-card"
+      >
         <Link
           to={ { pathname: `/item/${id}`, data: { searchData } } }
           data-testid="product-detail-link"
+          className="product-title"
         >
           <h1>{title}</h1>
         </Link>
-        <img src={ thumbnail } alt={ `img from ${title}` } />
-        <p>{price}</p>
+        <img
+          src={ thumbnail }
+          alt={ `img from ${title}` }
+          className="product-img"
+        />
+        <p className="product-price">{price}</p>
         <button
           data-testid="product-add-to-cart"
           type="button"
+          className="product-btn"
           onClick={ () => addToCart({ name: title, price }) }
         >
           Adicionar ao carrinho
